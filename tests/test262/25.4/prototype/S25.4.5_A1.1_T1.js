21493634f9c81881ded40d15b58e7a796a70e80f
Max Schlueter <mschlueter@uni-potsdam.de>
2017-09-26 11:51:28 +0200

    Add test262 tests for Promise.

---
+++ tests/test262/25.4/prototype/S25.4.5_A1.1_T1.js
@@ -0,0 +1,13 @@
+// Copyright 2014 Cubane Canada, Inc.  All rights reserved.
+// See LICENSE for details.
+
+/*---
+info: >
+    Promise prototype object is an ordinary object
+es6id: S25.4.5_A1.1_T1
+author: Sam Mikes
+description: Promise prototype does not have [[PromiseState]] internal slot
+negative: TypeError
+---*/
+
+Promise.call(Promise.prototype, function () {});
---
+++ tests/test262/25.4/resolve/S25.4.4.5_A1.1_T1.js
@@ -0,0 +1,18 @@
+// Copyright 2014 Cubane Canada, Inc.  All rights reserved.
+// See LICENSE for details.
+
+/*---
+info: >
+   Promise.resolve
+es6id: S25.4.4.5_A1.1_T1
+author: Sam Mikes
+description: Promise.resolve is a function
+---*/
+
+if ((typeof Promise.resolve) !== "function") {
+    $ERROR("Expected Promise.resolve to be a function");
+}
+
+if (Promise.resolve.length !== 1) {
+    $ERROR("Expected Promise.resolve to be a function of one argument");
+}
