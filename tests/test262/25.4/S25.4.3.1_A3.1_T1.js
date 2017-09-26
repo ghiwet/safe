5fef52d5022e892f27e5a2ba2b3ac775ba5eedc5
Max Schlueter <mschlueter@uni-potsdam.de>
2017-09-26 11:51:28 +0200

    Add test262 tests for Promise.

---
+++ tests/test262/25.4/S25.4.3.1_A3.1_T1.js
@@ -0,0 +1,13 @@
+// Copyright 2014 Cubane Canada, Inc.  All rights reserved.
+// See LICENSE for details.
+
+/*---
+info: >
+    Promise throws TypeError when executor is not callable
+es6id: S25.4.3.1_A3.1_T1
+author: Sam Mikes
+description: new Promise("not callable") throws TypeError
+negative: TypeError
+---*/
+
+new Promise("not callable");
