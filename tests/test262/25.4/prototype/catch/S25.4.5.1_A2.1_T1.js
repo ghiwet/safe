c32b0191453323b6ab5015f4556b59e2c90e9e4f
Max Schlueter <mschlueter@uni-potsdam.de>
2017-11-16 14:22:22 +0100

    Promise.prototype.catch modeled and tested

--- tests/test262/25.4/prototype/catch/S25.4.5.1_A2.1_T1.js
+++ tests/test262/25.4/prototype/catch/S25.4.5.1_A2.1_T1.js
@@ -8,15 +8,20 @@ es6id: S25.4.5.1_A2.1_T1
 author: Sam Mikes
 description: catch is a method on a Promise
 ---*/
+var __result1 = true;
+var __result2 = true;
 
 var p = Promise.resolve(3);
 
 if (!(p.catch instanceof Function)) {
-    $ERROR("Expected p.catch to be a function");
+    //$ERROR("Expected p.catch to be a function");
+    __result1 = false;
 }
 
 if (p.catch.length !== 1) {
-    $ERROR("Expected p.catch to take one argument");
+    //$ERROR("Expected p.catch to take one argument");
+    __result2 = false;
 }
-
+var __expect1 = true;
+var __expect2 = true;
 
