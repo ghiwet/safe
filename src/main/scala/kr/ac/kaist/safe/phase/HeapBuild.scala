2e16f011718cd6305e8f9d46a591c0bc74fb6e1d
Max Schlueter <mschlueter@uni-potsdam.de>
2018-02-28 14:07:05 +0100

    Add exception edge from top-level to event loop function and return exit state(s) from it

--- src/main/scala/kr/ac/kaist/safe/phase/HeapBuild.scala
+++ src/main/scala/kr/ac/kaist/safe/phase/HeapBuild.scala
@@ -11,7 +11,7 @@
 
 package kr.ac.kaist.safe.phase
 
-import scala.util.{ Try, Success, Failure }
+import scala.util.{ Failure, Success, Try }
 import kr.ac.kaist.safe.{ LINE_SEP, SafeConfig }
 import kr.ac.kaist.safe.nodes.cfg.CFG
 import kr.ac.kaist.safe.util._
@@ -51,8 +51,10 @@ case object HeapBuild extends PhaseObj[CFG, HeapBuildConfig, (CFG, Semantics, Tr
     val sens = config.callsiteSensitivity * config.loopSensitivity
     val initTP = sens.initTP
     val entryCP = ControlPoint(cfg.globalFunc.entry, initTP)
+    
     val eventLoop = cfg.getAllFuncs.filter(_.name == "eventLoop").head
     cfg.addEdge(cfg.globalFunc.exit, eventLoop.entry)
+    cfg.addEdge(cfg.globalFunc.exitExc, eventLoop.exitExc)
 
     val worklist = Worklist(cfg)
     worklist.add(entryCP)
